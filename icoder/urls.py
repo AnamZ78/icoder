22aad56545c68092348ac2de5af5e93b3106bb14
Anam_Zahid <anam@jewarinternational.com>
2025-07-24 18:49:11 +0530

    done

--- icoder/urls.py
+++ icoder/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin.site.site_header="iCoder Admin"
+admin.site.site_title="iCoder Admin Panel"
+admin.site.index_title="Welcome to iCoder Admin Panel"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
